9800b5b91af4a0cde6334b91c35d8ea927022ed9
Tochi Mba <128209372+tochi-mba@users.noreply.github.com>
2026-09-21 17:25:35 +0100

    Route STOP through consolidated stop script

--- STOP.bat
+++ STOP.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0HARD_STOP.ps1"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Stop-PhoneMirror.ps1"
 pause
