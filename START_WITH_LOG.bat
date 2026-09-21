959d7b56835a23bc776d1182c9810843a03e0a92
Tochi Mba <128209372+tochi-mba@users.noreply.github.com>
2026-09-21 17:25:40 +0100

    Make foreground start clear persistent stop state

--- START_WITH_LOG.bat
+++ START_WITH_LOG.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
+if exist "%~dp0stop.flag" del /q "%~dp0stop.flag"
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Start-PhoneMirror.ps1" -Foreground
 pause
